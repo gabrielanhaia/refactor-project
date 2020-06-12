becc76a349bb98e38cb6da4f286c227317906504
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-06-12 11:59:57 -0300

    feature: Refactor test structure

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+/**
+ * Class TestCase
+ *
+ * @package App\Tests\Util
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
---
+++ tests/Unit/PHPUnitTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Tests\Unit;
+
+use App\Tests\TestCase;
+
+/**
+ * Class PHPUnitTest responsible for testing if PHPUnit is working properly.
+ *
+ * @package App\Tests\Unit
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class PHPUnitTest extends TestCase
+{
+    public function testPhpUnit()
+    {
+        $this->assertTrue(true);
+    }
+}
